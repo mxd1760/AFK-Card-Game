edd30390750c354626a047962e796f6f775ccd41
Marcus Doucette <Marz3587@gmail.com>
2021-04-05 16:22:31 -0400

    monday meeting

--- AFK_prototype_1/index.js
+++ AFK_prototype_1/index.js
@@ -7,20 +7,20 @@ const app = express();
 const PORT_NUM = 3000;
 
 // app setup
-app.use(express.static(path.join(__dirname,'public')));
-app.set('views',path.join(__dirname,'views'));
-app.set('view engine','ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.render('home');
     console.log('request to HOME');
 });
 
-app.get('*',(req,res)=>{
+app.get('*', (req, res) => {
     res.render('home');
 });
 
-app.listen(PORT_NUM,()=>{
+app.listen(PORT_NUM, () => {
     console.log(`Server started on port: ${PORT_NUM}`);
 });
 
